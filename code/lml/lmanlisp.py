966c0176b222330c4d4831c08d927f797c174d11
Pär Bohrarper <par.bohrarper@gmail.com>
2014-07-27 00:16:02 +0200

    Moved code around, and made a script that creates the solution folder

--- code/lml/lmanlisp.py
+++ code/lml/lmanlisp.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # This is adapted from Peter Norvig's minimal lisp:
---
+++ preparesubmit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p solution
+./code/lml/lmanlisp.py code/lml/ai_0.lml > solution/lambdaman.gcc
