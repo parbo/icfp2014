9cef8066dbcd23985171a37e6007f4212a9c72ee
Pär Bohrarper <par.bohrarper@gmail.com>
2014-07-27 20:47:17 +0200

    Use ai_1.lml in submit script

--- preparesubmit.sh
+++ preparesubmit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p solution
-./code/lml/lmanlisp.py code/lml/ai_0.lml > solution/lambdaman.gcc
+./code/lml/lmanlisp.py code/lml/ai_1.lml > solution/lambdaman.gcc
 ./code/ghost/ghost.py code/ghost/ai_0.ghc > solution/ghost0.ghc
