a517b3efd510374d0b4284fd9cf5006a2b6fafe9
Dan Gerhardsson <dan_gerhardsson@hotmail.com>
2014-07-27 10:54:30 +0200

    Ghost parser refactoring.

--- code/ghost/ghost.py
+++ code/ghost/ghost.py
@@ -23,77 +23,109 @@ COMMAND_LINE = r'\s*(%s)(\s+(%s))?\s*(%s)?$' % (COMMAND, ARGS, COMMENT)
 LABEL_LINE = r'\s*(%s)\s*(%s)?$' % (LABEL, COMMENT)
 EMPTY_LINE = r'\s*(%s)?$' % COMMENT
 
-def parse_file(f):
-    cmds = []
-    labels = {}
-    for line_nbr, line in enumerate(f):
-        try:
-            parsed_line = parse_line(line)
-            line_type = parsed_line[0]
-            if line_type == 'COMMAND':
-                cmds.append(parsed_line)
-            elif line_type == 'LABEL':
-                label = parsed_line[1]
-                if label in labels:
-                    print 'Error: Reused label (%s) at line %d.' % (label, line_nbr)
-                    sys.exit(1)
-                labels[label] = len(cmds)
-        except ParseError as error:
-            print 'Error: Failed to parse line %d:' % line_nbr
-            print line
-            sys.exit(1)
-    return cmds, labels
+class Parser(object):
+    def __init__(self):
+        self.reset()
 
-def parse_line(line):
-    m = re.match(COMMAND_LINE, line)
-    if m:
-        return parse_command(m)
-    m = re.match(LABEL_LINE, line)
-    if m:
-        return parse_label(m)
-    m = re.match(EMPTY_LINE, line)
-    if m:
-        return 'EMPTY',
-    raise ParseError('Invalid line: %s' % line)
+    def reset(self):
+        self._lines = []
+        self._cmds = []
+        self._labels = {}
 
-def parse_command(match):
-    command = match.group(1).upper()
-    args = parse_args(match.group(2))
-    return 'COMMAND', command, args
+    def parse_file(self, f):
+        self.reset()
+        for line_nbr, line in enumerate(f):
+            try:
+                parsed_line = self.parse_line(line)
+                parsed_line['CODE'] = line.rstrip()
+                parsed_line['NBR'] = line_nbr
+                self._lines.append(parsed_line)
+                line_type = parsed_line['TYPE']
+                if line_type == 'COMMAND':
+                    self._cmds.append(parsed_line)
+                elif line_type == 'LABEL':
+                    label = parsed_line['LABEL']
+                    if label in self._labels:
+                        print 'Error: Reused label (%s) at line %d.' % (label, line_nbr)
+                        sys.exit(1)
+                    self._labels[label] = len(self._cmds)
+            except ParseError as error:
+                print 'Error: Failed to parse line %d:' % line_nbr
+                print line
+                sys.exit(1)
 
-def parse_args(arg_str):
-    args = []
-    if arg_str is not None:
-        args = [parse_arg(arg) for arg in arg_str.split(',')]
-    return tuple(args)
+    def parse_line(self, line):
+        m = re.match(COMMAND_LINE, line)
+        if m:
+            return self.parse_command(m)
+        m = re.match(LABEL_LINE, line)
+        if m:
+            return self.parse_label(m)
+        m = re.match(EMPTY_LINE, line)
+        if m:
+            return self.parse_empty(m)
+        raise ParseError('Invalid line: %s' % line)
 
-def parse_arg(arg_str):
-    arg_str = arg_str.strip()
-    m = re.match(LABEL_ARG, arg_str)
-    if m:
-        return 'LABEL', m.group(0)[1:]
-    return 'ARG', arg_str
+    def parse_command(self, match):
+        command = match.group(1).upper()
+        args = self.parse_args(match.group(2))
+        return {'TYPE': 'COMMAND', 'COMMAND': command, 'ARGS': args}
 
-def parse_label(match):
-    label = match.group(1)[:-1]
-    return 'LABEL', label
-
-def generate_code(cmds, labels):
-    code = []
-    for cmd in cmds:
+    def parse_args(self, arg_str):
         args = []
-        for arg in cmd[2]:
-            if arg[0] == 'LABEL':
-                args.append(str(labels[arg[1]]))
-            else:
-                args.append(arg[1])
-        code.append('%s %s' % (cmd[1], ','.join(args)))
-    return '\n'.join(code)
+        if arg_str is not None:
+            args = [self.parse_arg(arg) for arg in arg_str.split(',')]
+        return args
+
+    def parse_arg(self, arg_str):
+        arg_str = arg_str.strip()
+        m = re.match(REGISTER_ARG, arg_str)
+        if m:
+            register = m.group(0).lower()
+            return {'TYPE': 'REGISTER', 'STR': register, 'REGISTER': register}
+        m = re.match(INDIRECT_ARG, arg_str)
+        if m:
+            indirect = m.group(0).lower()
+            return {'TYPE': 'INDIRECT', 'STR': indirect, 'REGISTER': indirect[1]}
+        m = re.match(MEMORY_ARG, arg_str)
+        if m:
+            memory = m.group(0)
+            address = int(memory[1:-1])
+            return {'TYPE': 'MEMORY', 'STR': memory, 'ADDRESS': address}
+        m = re.match(CONSTANT_ARG, arg_str)
+        if m:
+            constant = m.group(0)
+            return {'TYPE': 'CONSTANT', 'STR': constant, 'CONSTANT': int(constant)}
+        m = re.match(LABEL_ARG, arg_str)
+        if m:
+            label = m.group(0)[1:]
+            return {'TYPE': 'LABEL', 'STR': label, 'LABEL': label}
+        raise ParseError('Invalid argument: %s' % arg_str)
+
+    def parse_label(self, match):
+        label = match.group(1)[:-1]
+        return {'TYPE': 'LABEL', 'LABEL': label}
+
+    def parse_empty(self, match):
+        return {'TYPE': 'EMPTY'}
+
+    def generate_code(self):
+        code = []
+        for cmd in self._cmds:
+            args = []
+            for arg in cmd['ARGS']:
+                if arg['TYPE'] == 'LABEL':
+                    args.append(str(self._labels[arg['LABEL']]))
+                else:
+                    args.append(arg['STR'])
+            code.append('%s %s' % (cmd['COMMAND'], ','.join(args)))
+        return '\n'.join(code)
     
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         with open(sys.argv[1], 'r') as f:
-            cmds, labels = parse_file(f)
-            print generate_code(cmds, labels)
+            parser = Parser()
+            parser.parse_file(f)
+            print parser.generate_code()
     else:
         print 'usage: %s input_file' % os.path.basename(sys.argv[0])
--- code/ghost/test_ghost.py
+++ code/ghost/test_ghost.py
@@ -5,65 +5,72 @@ import unittest
 import ghost
 
 class TestGhost(unittest.TestCase):
+    def setUp(self):
+        self.p = ghost.Parser()
+
     def test_parse_comment(self):
-        result = ghost.parse_line(' ; Comment')
-        self.assertEqual(result[0], 'EMPTY')
+        result = self.p.parse_line(' ; Comment')
+        self.assertEqual(result['TYPE'], 'EMPTY')
         
     def test_parse_label(self):
-        result = ghost.parse_line('label: ; Label')
+        result = self.p.parse_line('label: ; Label')
         self.assertEqual(len(result), 2)
-        self.assertEqual(result[0], 'LABEL')
-        self.assertEqual(result[1], 'label')
+        self.assertEqual(result['TYPE'], 'LABEL')
+        self.assertEqual(result['LABEL'], 'label')
         
     def test_parse_invalid_label(self):
         with self.assertRaises(ghost.ParseError) as error:
-            result = ghost.parse_line('2label: ; Invalid label')
+            result = self.p.parse_line('2label: ; Invalid label')
             
     def test_parse_cmd_no_args(self):
-        result = ghost.parse_line('HLT ; No arguments')
+        result = self.p.parse_line('HLT ; No arguments')
         self.assertEqual(len(result), 3)
-        self.assertEqual(result[0], 'COMMAND')
-        self.assertEqual(result[1], 'HLT')
-        args = result[2]
+        self.assertEqual(result['TYPE'], 'COMMAND')
+        self.assertEqual(result['COMMAND'], 'HLT')
+        args = result['ARGS']
         self.assertEqual(len(args), 0)
         
     def test_parse_cmd_single_arg(self):
-        result = ghost.parse_line('INC a; Single argument')
+        result = self.p.parse_line('INC a; Single argument')
         self.assertEqual(len(result), 3)
-        self.assertEqual(result[0], 'COMMAND')
-        self.assertEqual(result[1], 'INC')
-        args = result[2]
+        self.assertEqual(result['TYPE'], 'COMMAND')
+        self.assertEqual(result['COMMAND'], 'INC')
+        args = result['ARGS']
         self.assertEqual(len(args), 1)
-        self.assertEqual(args[0][0], 'ARG')
-        self.assertEqual(args[0][1], 'a')
+        self.assertEqual(args[0]['TYPE'], 'REGISTER')
+        self.assertEqual(args[0]['REGISTER'], 'a')
         
     def test_parse_cmd_multiple_args(self):
-        result = ghost.parse_line('JEQ 0,1,2; Multiple arguments')
+        result = self.p.parse_line('JEQ 0,1,2; Multiple arguments')
         self.assertEqual(len(result), 3)
-        self.assertEqual(result[0], 'COMMAND')
-        self.assertEqual(result[1], 'JEQ')
-        args = result[2]
+        self.assertEqual(result['TYPE'], 'COMMAND')
+        self.assertEqual(result['COMMAND'], 'JEQ')
+        args = result['ARGS']
         self.assertEqual(len(args), 3)
         for i in range(3):
-            self.assertEqual(args[i][0], 'ARG')
-            self.assertEqual(args[i][1], str(i))
+            self.assertEqual(args[i]['TYPE'], 'CONSTANT')
+            self.assertEqual(args[i]['CONSTANT'], i)
             
     def test_parse_too_many_args(self):
         with self.assertRaises(ghost.ParseError) as error:
-            result = ghost.parse_line('JEQ 0,1,2,3; Too many arguments')
+            result = self.p.parse_line('JEQ 0,1,2,3; Too many arguments')
             
     def test_parse_label_arg(self):
-        result = ghost.parse_line('JEQ #label,1,2; Label argument')
+        result = self.p.parse_line('JEQ #label,1,2; Label argument')
         self.assertEqual(len(result), 3)
-        self.assertEqual(result[0], 'COMMAND')
-        self.assertEqual(result[1], 'JEQ')
-        args = result[2]
+        self.assertEqual(result['TYPE'], 'COMMAND')
+        self.assertEqual(result['COMMAND'], 'JEQ')
+        args = result['ARGS']
         self.assertEqual(len(args), 3)
-        self.assertEqual(args[0][0], 'LABEL')
-        self.assertEqual(args[0][1], 'label')
+        self.assertEqual(args[0]['TYPE'], 'LABEL')
+        self.assertEqual(args[0]['LABEL'], 'label')
         for i in range(1, 3):
-            self.assertEqual(args[i][0], 'ARG')
-            self.assertEqual(args[i][1], str(i))
+            self.assertEqual(args[i]['TYPE'], 'CONSTANT')
+            self.assertEqual(args[i]['CONSTANT'], i)
+
+    def test_invalid_arg(self):
+        with self.assertRaises(ghost.ParseError) as error:
+            result = self.p.parse_line('INC [ab]; Invalid arguments')
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestGhost)
